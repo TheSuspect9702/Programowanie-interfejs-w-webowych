f943621847d0d1945a8279505dde05756191ce31
Dawid Pawłowski <263937@student.pwr.edu.pl>
2024-06-04 00:17:54 +0200

    modified code for React 1 to get 4.0

--- lab3/src/Components/HomePage.jsx
+++ lab3/src/Components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BrowseHotels from './BrowseHotels';
 
 function HomePage() {
   return (
@@ -18,57 +19,7 @@ function HomePage() {
         </article>
         <div class="hero-image-container"></div>
     </section>
-    <section id="browse" class="browse-section">
-        <p class="title-middle">Explore the hotels</p>
-        <input class="searchbar" placeholder="Search by hotel name, place etc."/>
-        <section class="grid hotel-cards">
-            <article class="hotel-card">
-                <div class="card-image">
-                    <p class="chip">Florence</p>
-                </div>
-                <p class="text-middle">Harmony Hideaway Hotel</p>
-                <p class="text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus quis felis a venenatis. Suspendisse accumsan aliquam lorem, sit amet ultricies justo tristique nec.</p>
-                <div class="hotel-card-footer">
-                    <p class="text-middle">★★★★★</p>
-                    <p class="text-middle">100€/room</p>
-                </div>
-            </article>
-            <article class="hotel-card">
-                <div class="card-image">
-                    <p class="chip">Florence</p>
-                </div>
-                <p class="text-middle">Harmony Hideaway Hotel</p>
-                <p class="text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus quis felis a venenatis. Suspendisse accumsan aliquam lorem, sit amet ultricies justo tristique nec.</p>
-                <div class="hotel-card-footer">
-                    <p class="text-middle">★★★★★</p>
-                    <p class="text-middle">100€/room</p>
-                </div>
-            </article>
-            <article class="hotel-card">
-                <div class="card-image">
-                    <p class="chip">Florence</p>
-                </div>
-                <p class="text-middle">Harmony Hideaway Hotel</p>
-                <p class="text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus quis felis a venenatis. Suspendisse accumsan aliquam lorem, sit amet ultricies justo tristique nec.</p>
-                <div class="hotel-card-footer">
-                    <p class="text-middle">★★★★★</p>
-                    <p class="text-middle">100€/room</p>
-                </div>
-            </article>
-            <article class="hotel-card">
-                <div class="card-image">
-                    <p class="chip">Florence</p>
-                </div>
-                <p class="text-middle">Harmony Hideaway Hotel</p>
-                <p class="text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus quis felis a venenatis. Suspendisse accumsan aliquam lorem, sit amet ultricies justo tristique nec.</p>
-                <div class="hotel-card-footer">
-                    <p class="text-middle">★★★★★</p>
-                    <p class="text-middle">100€/room</p>
-                </div>
-            </article>
-        </section>
-        <button class="button secondary">Find more <img src="./Assets/Arrow.svg"/></button>
-    </section>
+    <BrowseHotels></BrowseHotels>
     <section id="rent" class="footer grid">
         <div class="card-image"></div>
         <article class="footer-details">
